4313f6ec7c6815a54f901aa655ffb6b73bfc2fd1
Deepakmuru <deepakmurugesan01@gmail.com>
2026-07-29 20:04:10 +0530

    Initial commit: Maven + Selenium + JUnit5 setup with first passing test

---
+++ src/test/java/com/deepak/automation/tests/HomePageTest.java
@@ -0,0 +1,35 @@
+package com.deepak.automation.tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePageTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    void titleTest() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/");
+        String url = driver.getCurrentUrl();
+        String title = driver.getTitle();
+        Thread.sleep(2000);
+        System.out.println(url);
+        System.out.println(title);
+        Assertions.assertEquals("The Internet", title);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
